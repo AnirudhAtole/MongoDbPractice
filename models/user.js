c242e38aea3bae70407c58c98965196207c8374b
AnirudhAtole <anirudhatole@gmail.com>
2023-06-13 18:57:57 +0530

    added delete functionality

--- models/user.js
+++ models/user.js
@@ -28,15 +28,45 @@ class User {
       updatedCartItems[cartProductIndex].quantity = newQuantity;
     }
     else{
-      updatedCartItems.push({productId : new ObjectId(product._id) , quantity : 1})
+      updatedCartItems.push({productId : new ObjectId(product._id) , quantity : newQuantity})
     }
-    
-    updatedCartItems
+     
     const updatedCart = {items:updatedCartItems};
     const db = getDb();
     db.collection('users').updateOne({_id : new ObjectId(this._id)} , {$set : {cart : updatedCart}});
 
   }
+
+  getCart(){
+    const db = getDb();
+    const productIds = this.cart.items.map(i =>{
+      return i.productId;
+    })
+    return db.collection('products').find({_id: {$in : productIds}})
+    .toArray()
+    .then(products =>{
+      return products.map(p =>{
+        return {...p , quantity : this.cart.items.find(i =>{
+          return i.productId.toString() === p._id.toString();
+        }).quantity
+      }
+      })
+    });
+  }
+
+deleteItem(productId){
+  const db = getDb();
+  const updatedCartItems = this.cart.items.filter(item =>{
+    return item.productId.toString() !== productId.toString();
+  })
+  return db
+  .collection('users')
+  .updateOne(
+    {_id : new ObjectId(this._id)},
+    {$set : {cart : {items : updatedCartItems}}}
+  );
+}
+
 
   static findById(userId) {
     console.log(userId)
