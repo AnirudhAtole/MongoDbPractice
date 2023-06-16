7c39b2ba88849dc1b650c0c9f053c7f148929b6e
AnirudhAtole <anirudhatole@gmail.com>
2023-06-16 22:23:34 +0530

    Using mongoose and doing CRUD

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 const pathInfo = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
-const User = require('./models/user');
+// const User = require('./models/user');
 
 const app = express();
 
@@ -18,20 +18,27 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(pathInfo.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById('64844db4ca750bbb8f0c276c')
-    .then(user => {
-      req.user = new User(user.name, user.email , user.cart , user._id);
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById('64844db4ca750bbb8f0c276c')
+//     .then(user => {
+//       req.user = new User(user.name, user.email , user.cart , user._id);
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
+mongoose
+.connect(`mongodb+srv://Anirudh:${process.env.PASSWORD}@shop.i3qgdnl.mongodb.net/shop?retryWrites=true&w=majority`)
+.then(result =>{
+    app.listen(3000);
+    console.log("connected")
+  })
+.catch(err =>{
+  console.log(err);
+})
+
