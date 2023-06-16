accb883b591479d05f00bbc1c7bf1384ceb83eb1
AnirudhAtole <anirudhatole@gmail.com>
2023-06-16 16:17:10 +0530

    adding order and order details

--- models/user.js
+++ models/user.js
@@ -67,6 +67,40 @@ deleteItem(productId){
   );
 }
 
+addOrder(){
+  const db = getDb();
+  return this.getCart()
+  .then(products =>{
+    console.log(products)
+    const order ={
+      items : products,
+      user :{
+        _id : new ObjectId(this._id),
+        name : this.name,
+      }
+    };
+    return db
+    .collection('orders')
+    .insertOne(order);
+  })
+  .then(result => {
+    this.cart = { items : []}
+    return db.collection('users')
+    .updateOne(
+      { _id : new ObjectId(this._id)},
+      { $set : { cart: { items : [] }}}
+    );
+  });
+}
+
+getOrders(){
+  const db = getDb();
+  return db
+  .collection('orders')
+  .find({'user._id': new ObjectId(this._id)})
+  .toArray();
+}
+
 
   static findById(userId) {
     console.log(userId)
@@ -83,5 +117,6 @@ deleteItem(productId){
       });
   }
 }
+
 
 module.exports = User;
